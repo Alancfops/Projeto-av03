3b5ab5f288987d407bea2381c753b73b448b6fc7
Pedro Rosa <devpedrorosa@gmail.com>
2023-12-02 16:55:17 -0300

    test: wtf vs code python

---
+++ functions/menuAnotacao.py
@@ -0,0 +1,12 @@
+while True:
+  def menu_anotacao():
+    print("=" *100)
+    print("")
+    print("Menu:")
+    print("1 - Inserir")
+    print("2 - Listar")
+    print("3 - Editar")
+    print("4 - Deletar")
+    print("5 - Sair")
+    print("")
+    print("=" *100)
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from functions.menu import menu_usuario
 from functions.criptografia import getpass
 from Crud.usuario import *
-
-
+from functions.menuAnotacao import menu_anotacao
 
 while True:
     menu_usuario()
@@ -11,15 +10,19 @@ while True:
     if op == '1':
         username = input("Digite o nome de usuario:")
         password = getpass()
-        cadastrar( username, password)
-        print("Cadastrado com sucesso!")
 
+        if check_existing_user(username):
+            print("Nome de usuario ja existe!")
+        else:
+            cadastrar(username, password)
+            print("Cadastrado com sucesso!")
     elif op == '2':
         username = input("Digite o nome de usuario:")
         password = getpass()
         login_result = login( username, password)
         if login_result:
             print("Login bem-sucedido!")
+            menu_anotacao()
         else:
             print("Falha no login.")
 
